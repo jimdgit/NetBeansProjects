37e78a146abf207f0fd0450f8742e1095747b0cd
jimdgit <git@jimdonelson.com>
2016-11-26 15:05:54 -0500

    Initial comment

---
+++ WebApplication1/src/java/com/web/jim/NewServlet1.java
@@ -0,0 +1,140 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.web.jim;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.sql.*;  // for standard JDBC programs
+
+/**
+ *
+ * @author jcdonelson
+ */
+@WebServlet(name = "NewServlet1", urlPatterns = {"/NewServlet1"})
+
+public class NewServlet1 extends HttpServlet {
+
+    private Connection conn = null;
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        Statement stmt = null;
+        ResultSet rs = null;
+
+        try {
+
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery("SELECT * FROM customer");
+
+        } catch (SQLException ex) {
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+
+        }
+
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+
+            out.println("<title>Servlet NewServlet1</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet JDBC Direct NewServlet1 at " + request.getContextPath() + "</h1>");
+            out.println("<table style=\"width:100%\">");
+            try {
+                while (null != rs && rs.next()) {
+                    out.println(" <tr> <td>" + rs.getInt("customer_id") + "</td><td>" + rs.getString("name") + "</td>");
+                }
+            } catch (SQLException ex) {
+                System.out.println("SQLException: " + ex.getMessage());
+                System.out.println("SQLState: " + ex.getSQLState());
+            }
+            out.println("</table><p> " + "</p>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+    final private String connParms = "jdbc:mysql://localhost/sample?"
+            + "user=root&password=china999";
+
+    @Override
+    public void init() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            try {
+                conn = DriverManager.getConnection(connParms);
+                System.out.println("Connection OK");
+
+            } catch (SQLException ex) {
+
+                System.out.println("SQLException: " + ex.getMessage());
+                System.out.println("SQLState: " + ex.getSQLState());
+                System.out.println("VendorError: " + ex.getErrorCode());
+            }
+        } catch (Exception ex) {
+
+            System.out.println("Driver load exception");
+        }
+    }
+
+}
